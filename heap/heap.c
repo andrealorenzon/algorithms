df216b7f0d6c4e5edf850d0201e91e2350c37826
Andrea <andrelorenzon@gmail.com>
2019-11-06 15:59:14 +0100

    added main heap methods. to do: heapify

--- heap/heap.c
+++ heap/heap.c
@@ -29,3 +29,62 @@ void free_heap(BinaryHeap * H)
 { 
     free(H->heap); 
 }
+
+// root is the first element of the heap
+int root()
+{ 
+    return 0; 
+}
+
+
+// last is, well, the last element...
+int last(BinaryHeap * H) 
+{ 
+    return H->size - 1;
+}
+
+// left child of [i] is [2i+1]
+int left(int i) 
+{ 
+    return 2 * i + 1; 
+}
+
+// right child of [i] is [2i+2]
+int right(int i) 
+{ 
+    return 2 * i + 2; 
+}
+
+// parent of [i] is [(i-1)/2], that floors automatically
+int parent(int i) 
+{ 
+    return (i - 1) / 2;
+}
+
+
+// check if [i] is [0], returns 1 if it is, and other otherwise
+int is_root(int i) 
+{ 
+    return i == root();
+}
+
+
+// checks if [i] is a valid index (<= of array size)
+int is_valid(BinaryHeap * H, int i) 
+{ 
+    return H->size > i;
+}
+
+// returns the min (that is root).
+int heap_min(BinaryHeap * H) 
+{ 
+    return H->heap[root()];
+}
+
+// swaps two elements in the heap array, used in heapify
+void heap_swap(BinaryHeap * H, size_t i, size_t m)
+{
+  int tmp = H->heap[i];
+  H->heap[i] = H->heap[m];
+  H->heap[m] = tmp;
+}
--- heap/heap.h
+++ heap/heap.h
@@ -33,4 +33,33 @@ BinaryHeap build_heap(int * a, size_t size_a, int (*fcomp)(int,int));
 // destructor
 void free_heap(BinaryHeap * H);
 
+// root is the first element of the heap
+int root();
+
+// last is, well, the last element...
+int last(BinaryHeap * H);
+
+// left child of [i] is [2i+1]
+int left(int i); 
+
+// right child of [i] is [2i+2]
+int right(int i);
+
+// parent of [i] is [(i-1)/2], that floors automatically
+int parent(int i);
+
+// check if [i] is [0], returns 1 if it is, and other otherwise
+int is_root(int i);
+
+// checks if [i] is a valid index (<= of array size)
+int is_valid(BinaryHeap * H, int i);
+
+// returns the min (that is root).
+int heap_min(BinaryHeap * H);
+
+// swaps two elements in the heap array, used in heapify
+void heap_swap(BinaryHeap * H, size_t i, size_t m);
+
+
+
 #endif // HEAP_H__
